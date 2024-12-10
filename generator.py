3c345a86a2a4f04f1a121df1b151c1f1ac0df43c
John Shuford <johnshuford@me.com>
2024-12-10 16:50:10 -0700

    user input and using the string library

--- generator.py
+++ generator.py
@@ -1,18 +1,22 @@
 import random
 import string
 
+numOfCharacters = input("How many characters would you like your password to be?")
+
 password01 = []
 
-for x in range(4):
-  num = random.randint(0, 9)
+for x in range(int(numOfCharacters)):
+  num = random.choice(string.digits)
   password01.append(num)
 
 password = ''.join(str(x) for x in password01)
 
 print(password)
 
+'''
 string.ascii_lowercase
 string.ascii_uppercase
 string.digits
 string.punctuation
 random.choice(string.ascii_letters)
+'''
