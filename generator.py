88190dc9ea0f001f6dbffe58e28779c1b1c66671
John Shuford <johnshuford@me.com>
2024-12-10 16:43:00 -0700

    Importing random and string libraries... collecting puzzle pieces

--- generator.py
+++ generator.py
@@ -1,1 +1,18 @@
-print("I am a working piece of code!")
+import random
+import string
+
+password01 = []
+
+for x in range(4):
+  num = random.randint(0, 9)
+  password01.append(num)
+
+password = ''.join(str(x) for x in password01)
+
+print(password)
+
+string.ascii_lowercase
+string.ascii_uppercase
+string.digits
+string.punctuation
+random.choice(string.ascii_letters)
