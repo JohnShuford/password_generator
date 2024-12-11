c8d2562514def2e9dc74683f55ae6af1117444f6
John Shuford <johnshuford@me.com>
2024-12-11 11:47:29 -0700

    Finished the Generator! Added in all character types Added in user input on what type of characters they want Randomized the password based on the legnth and type of characters the user is looking for! Commented all sections of code for posterity.

--- generator.py
+++ generator.py
@@ -1,22 +1,50 @@
+#import needed libraries
 import random
 import string
 
+#create variables
+emptyPassword = []
+charType = []
+
+#verify wanted legnth of the password
 numOfCharacters = input("How many characters would you like your password to be?")
 
-password01 = []
+#verify the types of characters the user wants in their password
+wantLower = input("Would you like lower case letters? (y/n)")
+if wantLower == 'y':
+  charType.append('lower')
 
+wantUpper = input("Would you like upper case letters? (y/n)")
+if wantUpper == 'y':
+  charType.append('upper')
+
+wantNums = input("Would you like digits? (y/n)")
+if wantNums == 'y':
+  charType.append('nums')
+
+wantSpecialCharacters = input("Would you like special characters? (y/n)")
+if wantSpecialCharacters == 'y':
+  charType.append('punctuation')
+
+#create the random password
 for x in range(int(numOfCharacters)):
-  num = random.choice(string.digits)
-  password01.append(num)
+  #randomize the character type
+  randCharType = random.choice(charType)
 
-password = ''.join(str(x) for x in password01)
+  #randomize the character value based on the random character
+  if randCharType == 'lower' :
+    char = random.choice(string.ascii_lowercase)
+  elif randCharType == 'upper' :
+    char = random.choice(string.ascii_uppercase)
+  elif randCharType == 'nums' :
+    char = random.choice(string.digits)
+  else :
+    char = random.choice(string.punctuation)
+
+  #append the character to the password list
+  emptyPassword.append(char)
+
+#conver the list to a string
+password = ''.join(str(x) for x in emptyPassword)
 
 print(password)
-
-'''
-string.ascii_lowercase
-string.ascii_uppercase
-string.digits
-string.punctuation
-random.choice(string.ascii_letters)
-'''
